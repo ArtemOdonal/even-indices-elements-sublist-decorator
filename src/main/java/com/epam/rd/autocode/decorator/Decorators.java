315707ca620a6049c89485fc40045d19969c0ecb
ArtemOdonal <torvald2506@gmail.com>
2022-08-04 12:30:41 +0300

    Solved

--- src/main/java/com/epam/rd/autocode/decorator/Decorators.java
+++ src/main/java/com/epam/rd/autocode/decorator/Decorators.java
@@ -2,8 +2,11 @@ package com.epam.rd.autocode.decorator;
 
 import java.util.List;
 
-public class Decorators {
-    public static List<String> evenIndexElementsSubList(List<String> sourceList) {
-        throw new UnsupportedOperationException();
+public class Decorators  {
+    public static List<String> evenIndexElementsSubList(List<String> sourceList)  {
+        return new EvenIndicesInList(sourceList);
+      // throw new UnsupportedOperationException();
+
     }
 }
+
